c841cca7a7a0ff054bffebf3e41a3b0876172480
Matias Diez <mdiez@udesa.edu.ar>
2021-04-07 19:28:40 -0300

    bla

--- data/zapatillas.js
+++ data/zapatillas.js
@@ -1,48 +1,56 @@
 const zapatillas = {
-    lista: [
+    list: [
     {
+        id:0,
         nombre: "Nike Air Joran 1 Mid Red",
         descripcionCorta: "Las zapatillas Nike Air Jordan 1 Mid Red se inspiran en las primeras AJ1 y ofrecen, a los amantes del...",
         descripcionLarga: "Las zapatillas Nike Air Jordan 1 Mid Red se inspiran en las primeras AJ1 y ofrecen, a los amantes del modelo el estilo retro de Jordan, la oportunidad de seguir los pasos hacia la grandeza. Colores con mucho estilo y un mix de materiales aportan el toque de novedad a un diseño ya conocido a nivel mundial. La tecnología Air te proporciona una pisada más suave en cada paso.",
         imagen: "../public/images/products/jordan.jpeg",
     },                      
     {
+        id:1,
         nombre: "Adidas Yeezy 350 Zebra",
         descripcionCorta: "Las Yeezy reciben el sobrenombre de Zebras y vienen en un clásico esquema de color blanco, negro y rojo...",
         descripcionLarga: "Las Yeezy reciben el sobrenombre de Zebras y vienen en un clásico esquema de color blanco, negro y rojo. Luciendo una parte superior Primeknit de base blanca con detalles en negro que emite un ambiente de rayas Zebra, SPLY-350 se muestra en los lados en rojo rematado por una suela acolchada BOOST translúcida.",
         imagen: "../public/images/products/yeezy350.jpeg",
     },
     {
+        id:2,
         nombre: "Dr.Martens Black",
         descripcionCorta: "La bota es la original de Dr. Martens. Su diseño es inconfundible: 8 ojales, el emblemático cuero suave...",
         descripcionLarga: "La bota es la original de Dr. Martens. Su diseño es inconfundible: 8 ojales, el emblemático cuero suave Smooth de Dr. Martens, la suela con cámara de aire y surcos laterales y el pespunte amarillo.",
         imagen: "../public/images/products/doctormartens.jpeg",
     },                     
     {
+        id:3,
         nombre: "Adidas Ojotas Adilette Black",
         descripcionCorta: "Relajáte después de la sesión de piscina con esta Ojotas ideales para la ducha. Su diseño sencillo muestra...",
         descripcionLarga: "Relajáte después de la sesión de piscina con esta Ojotas ideales para la ducha. Su diseño sencillo muestra el auténtico legado de adidas con las emblemáticas 3 Rayas. Incorporan una mediasuela cómoda y suave que reconforta los pies cansados.",
         imagen: "../public/images/products/adidas.jpeg",
     },
     {
+        id:4,
         nombre: "Vans Old Skool",
         descripcionCorta: "Las Old Skool son las zapatillas clásicas de Vans y el primer modelo en lucir el icónico sidestripe de la...",
         descripcionLarga: "Las Old Skool son las zapatillas clásicas de Vans y el primer modelo en lucir el icónico sidestripe de la marca. Son zapatillas de corte bajo confeccionadas con capellada de canvas y cuero suede resistente, tiene punteras reforzadas para añadir durabilidad, la suela de caucho original Vans Wafflesole y cuello acolchado para ofrecer sujeción y confort.",
         imagen: "../public/images/products/vans.jpg",
     },
     {
+        id:5,
         nombre: "Converse Chuck Taylor All-Star",
         descripcionCorta: "Originales desde 1917, las zapatillas Converse Chuck Taylor All Star son fácilmente identificables por su...",
         descripcionLarga: "Originales desde 1917, las zapatillas Converse Chuck Taylor All Star son fácilmente identificables por su inconfundible silueta y el característico patch en el tobillo.  Nacidas en una cancha de básquet, lograron trascender la escena deportiva para ser adoptadas por aquellos espíritus creativos como símbolo de expresión individual.",
         imagen: "../public/images/products/converse.jpeg",
     },
     {
+        id:6,
         nombre: "Botines Nike Superfly 7 Blue",
         descripcionCorta: "Los botines Nike Superfly 7 Academy nacieron para que te conviertas en el mejor dentro de la cancha. Un diseño...",
         descripcionLarga: "Los botines Nike Superfly 7 Academy nacieron para que te conviertas en el mejor dentro de la cancha. Un diseño exclusivo, audaz y cómodo te permitirán sentir el control y la precisión que necesitas, además de contar con un calzado liviano para cambiar tu ritmo de juego cuando lo necesites.",
         imagen: "../public/images/products/botines.jpeg",
     },
     {
+        id:7,
         nombre: "Alpargatas Stripes",
         descripcionCorta: "El clásico argentino con suela de yute que viene en una tela suave 100% algodón con rayas azules. Ideal para...",
         descripcionLarga: "El clásico argentino con suela de yute que viene en una tela suave 100% algodón con rayas azules. Ideal para el día y la noche.Este modelo viene con refuerzo en el talón para darle más calidad y durabilidad.",
