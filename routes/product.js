0893f44d6a4ebf00adef840b2cb9bd2d82e014c1
JoacoRebelo <joaco.rebelo@gmail.com>
2021-06-18 14:19:05 -0300

    intento fallido de borrar producto y msj de error

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,6 @@ let controller = require('../controllers/productController');
 
 router.get('/:id', controller.product);
 router.post('/:id', controller.comentarios);
-router.post('/', controller.borrar);
+router.post('/borrar', controller.borrar);
 
 module.exports = router;
